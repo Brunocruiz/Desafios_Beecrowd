c27515f6ae78dec2dfae681836e0efc7af8600f9
Bruno Martins da Cruz <bruno-martinslife@hotmail.com>
2024-02-29 14:39:40 -0300

    Create soma_simples.cs

---
+++ Iniciantes/soma_simples.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  static void Main(string[] args) {
+    int A = Convert.ToInt32(Console.ReadLine());
+    int B = Convert.ToInt32(Console.ReadLine());
+    
+    int SOMA = A + B;
+
+    Console.WriteLine("SOMA = {0}", SOMA);
+  }
+}
